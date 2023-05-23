6cc37a51b501259b4d3dc333d27bb7a4851195f1
Djameleddine BOULECHFAR <djamelboulechfar@gmail.com>
2023-05-23 13:41:33 +0200

    Optimisations

--- src/main/java/advent_of_code/year_2015/day_4/Main.java
+++ src/main/java/advent_of_code/year_2015/day_4/Main.java
@@ -2,32 +2,34 @@ package advent_of_code.year_2015.day_4;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.NoSuchElementException;
+
 public class Main {
     public static void main(String[] args) {
         String input = "iwrupvqb";
-
-        // Solution naive
+        
         int part1Answer = findLowestNumberForHashToStartWithNTimesGivenCharacter(input, "0", 5);
-        if (part1Answer != -1) System.out.println("Réponse partie 1 : " + part1Answer);
-        else System.out.println("Pas de solution trouvée pour la partie 1.");
+        System.out.println("Réponse partie 1 : " + part1Answer);
 
         int part2Answer = findLowestNumberForHashToStartWithNTimesGivenCharacter(input, "0", 6);
-        if (part2Answer != -1) System.out.println("Réponse partie 2 : " + part2Answer);
-        else System.out.println("Pas de solution trouvée pour la partie 2.");
+        System.out.println("Réponse partie 2 : " + part2Answer);
     }
 
     private static int findLowestNumberForHashToStartWithNTimesGivenCharacter(String input, String startCharacter, int n) {
         int i = 0;
+
+        String searched = startCharacter.repeat(n);
+
         while (i < Integer.MAX_VALUE) {
             String secretKey = input + i;
 
             String hash = DigestUtils.md5Hex(secretKey);
 
-            if (hash.startsWith(startCharacter.repeat(n))) return i;
+            if (hash.startsWith(searched)) return i;
 
             i++;
         }
 
-        return -1;
+        throw new NoSuchElementException("Pas de solution trouvée pour l'input " + input + ", le caractère '" + startCharacter + "' et n=" + n);
     }
 }
