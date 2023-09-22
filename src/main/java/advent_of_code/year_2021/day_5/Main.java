f739d69fe156ac6986d076f564ca4b0e97e76cf4
Djameleddine BOULECHFAR <djamelboulechfar@gmail.com>
2023-09-22 16:33:58 +0200

    :art: Utilisation d'un seul stream pour traiter l'exercice

--- src/main/java/advent_of_code/year_2021/day_5/Main.java
+++ src/main/java/advent_of_code/year_2021/day_5/Main.java
@@ -3,31 +3,34 @@ package advent_of_code.year_2021.day_5;
 import advent_of_code.utils.MyFileReader;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // Récupération de l'input sous forme de liste de String
         List<String> inputs = MyFileReader.readFileAndReturnStringList("src/main/java/advent_of_code/year_2021/day_5/input.txt");
 
-        // Construction d'une liste d'HydrothermalVent à partir des inputs grâce au constructeur prévu à cet effet
-        List<HydrothermalVent> hydrothermalVents = inputs.stream().map(HydrothermalVent::new).toList();
-
-        // Filtrage des HydrothermalVents qui sont en diagonale (à commenter pour la partie 2)
-        hydrothermalVents = hydrothermalVents.stream().filter(HydrothermalVent::isInStraightLine).toList();
-
-        // Création d'une map stockant pour chaque position le nombre d'HydrothermalVents qui y passent
-        Map<Position, Integer> positionsPassages = new HashMap<>();
-        for (HydrothermalVent hydrothermalVent : hydrothermalVents) {
-            for (Position position : hydrothermalVent.getLinePositions()) {
-                positionsPassages.merge(position, 1, Integer::sum);
-            }
-        }
-
-        // Compte les positions sur lesquelles plusieurs HydrothermalVents passent
-        long answer = positionsPassages.values().stream().filter(value -> value > 1).count();
+        long answer = inputs.stream()
+                // Construction d'une liste d'HydrothermalVent à partir des inputs grâce au constructeur prévu à cet effet
+                .map(HydrothermalVent::new)
+                // Filtrage des HydrothermalVents qui sont en diagonale (à commenter pour la partie 2)
+                .filter(HydrothermalVent::isInStraightLine)
+                // Récupération de toutes les Position de chaque HydrothermalVent sous forme de liste
+                .map(HydrothermalVent::getLinePositions)
+                // Création d'une unique liste contenant toutes les Position de toutes les HydrothermalVent
+                .flatMap(List::stream)
+                // Crée une map ayant pour clé une Position et pour valeur son nombre d'occurrences dans la liste précédente
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ))
+                // Filtre les entrées de la map dont la valeur est supérieure à 1, ce qui correspond aux positions sur
+                // lesquelles plusieurs HydrothermalVent passent
+                .values().stream()
+                .filter(value -> value > 1)
+                .count();
 
         System.out.println("Nombre de zones à éviter : " + answer);
     }
