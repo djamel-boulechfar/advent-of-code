68801979baf534a1ae657834d9d532599809be6c
Djamel Boulechfar <djamelboulechfar@gmail.com>
2024-02-13 16:34:17 +0100

    :bulb: Ajout de commentaires (ouais j'ai tryhard)

--- src/main/java/advent_of_code/year_2016/day_7/Main.java
+++ src/main/java/advent_of_code/year_2016/day_7/Main.java
@@ -5,15 +5,24 @@ import advent_of_code.utils.MyFileReader;
 import java.io.IOException;
 import java.util.List;
 
-public class Part1 {
+public class Main {
     public static void main(String[] args) throws IOException {
         List<String> input = MyFileReader.readFileAndReturnStringList("src/main/java/advent_of_code/year_2016/day_7/input.txt");
 
-        long answer = input.stream()
+        List<IpAddress> ipAddresses = input.stream()
                 .map(IpAddress::new)
+                .toList();
+
+        long part1Answer = ipAddresses.stream()
                 .filter(IpAddress::supportsTls)
                 .count();
 
-        System.out.println("Réponse : " + answer);
+        System.out.println("Réponse partie 1 : " + part1Answer);
+
+        long part2Answer = ipAddresses.stream()
+                .filter(IpAddress::supportsSsl)
+                .count();
+
+        System.out.println("Réponse partie 2 : " + part2Answer);
     }
 }
