3a9e4387557d81977af7b7f6222e4bc0a874cd66
Djamel Boulechfar <djamelboulechfar@gmail.com>
2024-10-21 10:13:25 +0200

    :art: Améliorations jour 8 2020

--- src/main/java/advent_of_code/year_2020/day_8/Main.java
+++ src/main/java/advent_of_code/year_2020/day_8/Main.java
@@ -3,10 +3,7 @@ package advent_of_code.year_2020.day_8;
 import advent_of_code.utils.MyFileReader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,56 +13,12 @@ public class Main {
                 .map(Operation::new)
                 .toList();
 
-        ExecutionResult executionResult = computeOperations(operations);
+        ExecutionResult executionResult = Operation.computeOperations(operations);
 
         System.out.println("Part 1 answer = " + executionResult.accumulator());
 
-        for (int i = 0; i < operations.size(); i++) {
-            Operation currentOperation = operations.get(i);
+        int accumulatorAfterCorrection = Operation.correctAndComputeOperations(operations);
 
-            if (OperationType.ACC.equals(currentOperation.getOperationType())) {
-                continue;
-            }
-
-            List<Operation> correctedOperations = new ArrayList<>(operations.stream()
-                    .map(Operation::copy)
-                    .toList());
-
-            correctedOperations.set(i, new Operation(currentOperation.getOperationType().correct(), currentOperation.getValue()));
-
-            ExecutionResult currentExecutionResult = computeOperations(correctedOperations);
-
-            if (currentExecutionResult.index() >= correctedOperations.size()) {
-                System.out.println("Part 2 answer = " + currentExecutionResult.accumulator());
-                break;
-            }
-        }
-    }
-
-    /**
-     * Computes given list of information and return accumulator value and last executed instruction index.
-     *
-     * @param operations Operations to execute.
-     * @return Accumulator value and last executed instruction index.
-     */
-    private static ExecutionResult computeOperations(List<Operation> operations) {
-        Set<Integer> executedOperationsIndexes = new HashSet<>();
-
-        int accumulator = 0;
-        int index = 0;
-        while (executedOperationsIndexes.add(index) && index < operations.size()) {
-            Operation currentOperation = operations.get(index);
-
-            switch (currentOperation.getOperationType()) {
-                case ACC -> {
-                    accumulator += currentOperation.getValue();
-                    index++;
-                }
-                case JMP -> index += currentOperation.getValue();
-                default -> index++;
-            }
-        }
-
-        return new ExecutionResult(accumulator, index);
+        System.out.println("Part 2 answer = " + accumulatorAfterCorrection);
     }
 }
--- src/main/java/advent_of_code/year_2020/day_8/Operation.java
+++ src/main/java/advent_of_code/year_2020/day_8/Operation.java
@@ -2,9 +2,11 @@ package advent_of_code.year_2020.day_8;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,5 +32,56 @@ public class Operation {
 
     public Operation copy() {
         return new Operation(this.getOperationType(), this.getValue());
+    }
+
+    /**
+     * Computes given list of information and return accumulator value and last executed instruction index.
+     *
+     * @param operations Operations to execute.
+     * @return Accumulator value and last executed instruction index.
+     */
+    public static ExecutionResult computeOperations(List<Operation> operations) {
+        Set<Integer> executedOperationsIndexes = new HashSet<>();
+
+        int accumulator = 0;
+        int index = 0;
+        while (executedOperationsIndexes.add(index) && index < operations.size()) {
+            Operation currentOperation = operations.get(index);
+
+            switch (currentOperation.getOperationType()) {
+                case ACC -> {
+                    accumulator += currentOperation.getValue();
+                    index++;
+                }
+                case JMP -> index += currentOperation.getValue();
+                default -> index++;
+            }
+        }
+
+        return new ExecutionResult(accumulator, index);
+    }
+
+    public static int correctAndComputeOperations(List<Operation> operations) {
+        for (int i = 0; i < operations.size(); i++) {
+            Operation currentOperation = operations.get(i);
+
+            if (OperationType.ACC.equals(currentOperation.getOperationType())) {
+                continue;
+            }
+
+            List<Operation> correctedOperations = new ArrayList<>(operations.stream()
+                    .map(Operation::copy)
+                    .toList());
+
+            correctedOperations.set(i, new Operation(currentOperation.getOperationType().correct(), currentOperation.getValue()));
+
+            ExecutionResult currentExecutionResult = Operation.computeOperations(correctedOperations);
+
+            if (currentExecutionResult.index() >= correctedOperations.size()) {
+                return currentExecutionResult.accumulator();
+            }
+        }
+
+        throw new IllegalArgumentException("WTF no possible correction.");
     }
 }
